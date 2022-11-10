600edf3fdc29b4bad857c7d886d166ca9155978b
Amir Mussa <amir.mussa99@gmail.com>
2022-11-09 18:15:03 -0600

    stuff

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,17 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <a href={link} target="_blank" rel="noreferrer">
+        <img src={imgUrl} alt="project"/>
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+        </a>
+      </div>
+    </Col>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,101 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+// import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import fative from "../assets/img/project-fative.png"
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Fative.io",
+      description: "Created to help families and kids in our community.",
+      imgUrl: fative,
+      link: "https://www.fative.io/",
+    },
+    {
+      title: "AirBnB Clone",
+      description: "Designed to challenge my CSS skills",
+      imgUrl: projImg2,
+      link: "https://codesandbox.io/s/airbnb-clone-pwlqcj?file=/src/icons.js"
+    },
+    {
+      title: "Banter",
+      description: "Open chatroom designed for friends",
+      imgUrl: projImg3,
+      link: "https://banter-rho.vercel.app/"
+    },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg1,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg2,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg3,
+    // },
+  ];
+
+  return (
+    <section className="projects" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility once>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Below are some of my favorite projects I've created to challenge myself, or contributed to that solve real world problems. </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInDown" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>Coming Soon!</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Coming Soon!</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
